a0455fcdc672a65d422d74320d8cc09474029be2
oanh dc23v7x314 <thuyoanh081196@gmail.com>
2025-04-12 16:58:05 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const cors = require("cors");
 const app = express();
 
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "welcome to contact book application." });
+  res.json({ message: "Welcome to the contact book application." });
 });
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+  next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
